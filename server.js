c0444700bd013bfd68eb139f3a77383007f38276
qrell2 <136986538+qrell2@users.noreply.github.com>
2024-07-19 11:42:00 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Welcome to my web site!</h1>');
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const text = body.trim();
+      if (text === 'Привет, сервер!') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Привет, клиент!');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Неизвестный запрос');
+      }
+    });
+  } else {
+    res.writeHead(405, {'Content-Type': 'text/html'});
+    res.end('<html><head><title>405 Not Allowed</title></head><body bgcolor="white"><center><h1>405 Not Allowed</h1></center></body></html>');
+  }
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Сервер запущен на порту 3000');
 });
